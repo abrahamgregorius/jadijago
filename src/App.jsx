825f28ac70de999b6874c8a62198ee605a3fc363
abrahamgregorius <athioii6@gmail.com>
2025-05-30 23:37:42 +0700

    feat: route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         {/* Guest route */}
         <Route element={<Home></Home>} path='/'></Route>
         <Route element={<Login></Login>} path='/login'></Route>
+        <Route element={<Register></Register>} path='/register'></Route>
       
       
         {/* User route */}
