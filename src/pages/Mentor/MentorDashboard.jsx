fc98d25a12fcd636b06dfb065a3dd800d83f4677
abrahamgregorius <athioii6@gmail.com>
2025-05-31 00:16:56 +0700

    feat: mentor and user dashboard page

--- src/pages/Mentor/MentorDashboard.jsx
+++ src/pages/Mentor/MentorDashboard.jsx
@@ -1,7 +1,12 @@
+import DashboardNavbar from "../../components/Dashboard/DashboardNavbar";
+import DashboardSidebar from "../../components/Dashboard/DashboardSidebar";
+
 export default function MentorDashboard() {
-    return (
-        <>
-        
-        </>
-    )
+  return (
+    <>
+      <DashboardNavbar></DashboardNavbar>
+      <DashboardSidebar></DashboardSidebar>
+      <div class="sm:ml-64 mt-14 p-4 text-black"></div>
+    </>
+  );
 }
