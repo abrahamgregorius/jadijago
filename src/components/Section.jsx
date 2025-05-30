cf1d4572cdcbf0dca177d323854d7b585bbb3ed2
abrahamgregorius <athioii6@gmail.com>
2025-05-30 22:46:27 +0700

    feat: components

---
+++ src/components/Section.jsx
@@ -0,0 +1,11 @@
+export default function Section() {
+  return (
+    <>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+          
+        </div>
+      </section>
+    </>
+  );
+}
