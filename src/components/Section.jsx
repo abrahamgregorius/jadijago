39155fe0d6f5e6d417db9696d0e044a51e768e1f
abrahamgregorius <athioii6@gmail.com>
2025-05-30 23:38:02 +0700

    feat: navbar, section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,7 @@ export default function Section() {
   return (
     <>
       <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center md:min-h-screen lg:py-16">
           
         </div>
       </section>
