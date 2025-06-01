bb5927924bdb504adc098922a0bdffc46ed0366d
abrahamgregorius <athioii6@gmail.com>
2025-06-01 23:39:30 +0700

    feat: add guest and user navbar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Main from "../components/Main";
+import { useEffect } from "react";
 
 export default function Login() {
+  const navigate = useNavigate()
+  
+  function login(e) {
+    e.preventDefault()
+    localStorage.setItem('token', 'login')
+    navigate('/')
+  }
+
+  useEffect(() => {
+    if(localStorage.getItem('token')) {
+      navigate(-1)
+      alert("Not allowed")
+    }
+  }, [])
+
   return (
     <>
       <Main>
@@ -83,8 +99,9 @@ export default function Login() {
                       Forgot password?
                     </Link> */}
                   </div>
-                  <Link to={"/user/dashboard"}>
+                  <Link to={"/"}>
                     <button
+                      onClick={(e) => login(e)}
                       type="submit"
                       className="text-white w-full cursor-pointer mb-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
