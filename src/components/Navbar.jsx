6c14ef36711ae37c5af1eb0c13d2b72abd215a8e
abrahamgregorius <athioii6@gmail.com>
2025-06-17 18:22:08 +0700

    feat: responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 
 export default function Navbar({ className }) {
   const [isLog, setIsLog] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   function logout() {
     localStorage.removeItem("token");
     setIsLog(false);
+  }
+
+  function toggleMobileMenu() {
+    setIsOpen(!isOpen);
   }
 
   useEffect(() => {
@@ -29,7 +34,21 @@ export default function Navbar({ className }) {
               JadiJago
             </span>
           </Link>
-          <div className="flex gap-2 md:order-2  md:space-x-0 rtl:space-x-reverse">
+          
+          <div className="flex sm:hidden">
+            <span
+              id="mobile-menu-button"
+              onClick={toggleMobileMenu}
+              className="text-white cursor-pointer"
+            >
+              &#9776;
+            </span>
+          </div>
+          {isOpen ? (
+          <div
+            className="flex flex-col pt-4 items-center justify-between w-full sm:w-auto"
+            id="mobile-menu"
+          >
             {isLog ? (
               <>
                 <Link to="/" className="text-white px-4 py-2 text-center">
@@ -50,36 +69,6 @@ export default function Navbar({ className }) {
                 >
                   My Courses
                 </Link>
-                <div>
-                  <form>
-                    <div class="relative">
-                      <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg
-                          class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 20 20"
-                        >
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                          />
-                        </svg>
-                      </div>
-                      <input
-                        type="search"
-                        id="search"
-                        class="block w-full ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Search in site"
-                        required
-                      />
-                    </div>
-                  </form>
-                </div>
                 <Link
                   onClick={logout}
                   className="text-white flex justify-center items-center px-4 py-2 text-center cursor-pointer"
@@ -98,6 +87,7 @@ export default function Navbar({ className }) {
               </>
             )}
           </div>
+          ) : null}
         </div>
       </nav>
     </>
